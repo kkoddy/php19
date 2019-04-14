6fd597b23ef38c056025bc84a8ae0c171a55748f
kkoddy <davidmoyatecnico@outook.com>
2019-04-14 03:27:34 +0200

    Empezamos maquetacion y primeras funciones sql

---
+++ ProyectoFinal/functions.php
@@ -0,0 +1,41 @@
+<?php
+/*Preparamos las consultas necesarias*/
+$host='localhost';
+$user='root';
+$pass='root';
+$database='restaurante';
+$link_connection="mysql:host=$host;dbname=$database;";
+
+
+try {
+    $pdo=new PDO($link_connection, $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $text_sql="SELECT usuario.nombre AS CLIENTE, comentarios.comentario AS COMENTARIO,"
+            . " producto.nombre AS PLATO, comentarios.valoracion AS VALORACION FROM comentarios, usuario, producto "
+            . "WHERE usuario.id_usu=comentarios.fk_usu AND producto.id_producto=comentarios.fk_pro AND"
+            . " comentarios.valoracion=(SELECT MAX(valoracion) FROM comentarios);";
+    $statement=$pdo->prepare($text_sql);
+    $statement->execute();
+    //echo "Se han recuperado ".$statement->rowCount()." entradas:\n";
+    $cliente="";
+    $plato="";
+    $comentario="";
+    $valoracion="";
+    while($filas_tabla=$statement->fetch(PDO::FETCH_ASSOC)){
+        //echo $filas_tabla['CLIENTE']." ".$filas_tabla['PLATO']." ".$filas_tabla['COMENTARIO']." ".$filas_tabla['VALORACION'];
+        $cliente=$filas_tabla['CLIENTE'];
+        $plato=$filas_tabla['PLATO'];
+        $comentario=$filas_tabla['COMENTARIO'];
+        $valoracion=$filas_tabla['VALORACION'];
+        
+        echo $cliente." ".$plato." ".$comentario." ".$valoracion;
+    }
+
+}
+catch(PDOException $e){
+    echo "Ha ocurrido un error". $e->getMessage();
+}
+finally{
+    $pdo=null;
+    //echo "Trabajo finalizado";
+}
---
+++ ProyectoFinal/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>Anapurna Gastrobar</title>
+        <meta name="Keywords" content="food"/>
+        <meta name="Description" content="Restaurante anapurna"/>
+        <link rel="stylesheet" href="css/style.css" type="text/css"/>
+        <script src="jquery-3.4.0.js" type="text/javascript"></script>
+        <script src="js/main.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <?php
+        include 'functions.php';
+        ?>
+        <div id="base">
+              <header>
+                <nav class="nav">
+               
+                  <ul>
+                    <li><a href="#">Bienvenido</a></li>
+                    <li><a href="#">Menú</a></li>
+                    <li><a href="#">Media</a></li>
+                    <li><a href="#">Nosotros</a></li>
+                    <li><a href="#">Login</a></li>
+                  </ul>
+                
+                </nav>
+     </header>
+     
+            <section class="main">
+            <h3>contenido principal</h3>
+                  <article class="article">
+                <header>
+                    <h2>Mejor valorado: <?php echo $plato; ?></h2>
+                    
+                </header>
+                
+                           </article>
+                     <article class="article">
+                <header>
+                    <h1><?php echo $plato; ?></h1>
+                    
+                </header>
+                
+                <div class="comment">
+                    
+                    <img src="images/food1.jpg"alt="art1">
+                    <p><?php echo $cliente."<br>".$comentario."<br> Valoración:".$valoracion;?> </p>
+                </div>
+       
+       
+         
+        </section>
+
+            
+        <footer>
+               <ul>
+                    <li><a href="#">Aviso Legal</a></li>
+                    <li><a href="#">Copyright 2019</a></li>
+                 
+                    <li><a href="#">Contacto</a></li>
+                  </ul>
+        </footer>
+        </div>
+    </body>
+</html>
